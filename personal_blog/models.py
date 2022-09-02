917d333a581dc8a440423388e0a488f484fa4538
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-09-02 09:22:28 +0545

    :zap: blog model created

--- personal_blog/models.py
+++ personal_blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE) #when author is deleted all the posts will also be deleted
+
+
+    def __str__(self):
+        return self.title
